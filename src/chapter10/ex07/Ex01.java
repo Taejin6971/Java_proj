35d9132c42beace4609fd152dbf20abe8cefac9e
Taejin Kim <skyrtc@gmail.com>
2023-05-24 10:17:20 +0900

    20230523

--- src/chapter10/ex07/Ex01.java
+++ src/chapter10/ex07/Ex01.java
@@ -44,28 +44,46 @@ public class Ex01 {
 	// 배열의 객체를 매개변수로 받아서 총 가격을 출력
 	void totalPrice(Object[] obj) {
 
-		int sumA = 0;
-		int sumB = 0;
-		int sumS = 0;
-		int allSum = 0;
-		for (int i = 0; i < obj.length; i++) {
-			if (obj[i] instanceof Apple) {
-				sumA = ((Apple) obj[i]).price * ((Apple) obj[i]).count;
-			}
-		}
-		for (int i = 0; i < obj.length; i++) {
-			if (obj[i] instanceof Banana) {
-				sumB = ((Banana) obj[i]).price * ((Banana) obj[i]).count;
-			}
-		}
-		for (int i = 0; i < obj.length; i++) {
-			if (obj[i] instanceof Strawberry) {
-				sumS = ((Strawberry) obj[i]).price * ((Strawberry) obj[i]).count;
-			}
-		}
-		allSum = sumA + sumB + sumS;
+//		int sumA = 0;
+//		int sumB = 0;
+//		int sumS = 0;
+//		int allSum = 0;
+//
+//		for (int i = 0; i < obj.length; i++) {
+//			if (obj[i] instanceof Apple) {
+//				sumA = ((Apple) obj[i]).price * ((Apple) obj[i]).count;
+//			}
+//		}
+//		for (int i = 0; i < obj.length; i++) {
+//			if (obj[i] instanceof Banana) {
+//				sumB = ((Banana) obj[i]).price * ((Banana) obj[i]).count;
+//			}
+//		}
+//		for (int i = 0; i < obj.length; i++) {
+//			if (obj[i] instanceof Strawberry) {
+//				sumS = ((Strawberry) obj[i]).price * ((Strawberry) obj[i]).count;
+//			}
+//		}
+//		allSum = sumA + sumB + sumS;
+//
+//		System.out.println("모든 과일의 촐 가격은 " + allSum + " 원 입니다.");
 
-		System.out.println("모든 과일의 촐 가격은 " + allSum + " 원 입니다.");
+		int[] sum = new int[3]; // { a1, b1, s1 }
+
+		if (obj[0] instanceof Apple) {
+			sum[0] = ((Apple) obj[0]).price * ((Apple) obj[0]).count;
+		}
+		if (obj[1] instanceof Banana) {
+			sum[1] = ((Banana) obj[1]).price * ((Banana) obj[1]).count;
+		}
+		if (obj[2] instanceof Strawberry) {
+			sum[2] = ((Strawberry) obj[2]).price * ((Strawberry) obj[2]).count;
+		}
+
+		System.out.println("사과의 합은 : " + sum[0]);
+		System.out.println("바나나의 합은 : " + sum[1]);
+		System.out.println("딸기의 합은 : " + sum[2]);
+		System.out.println("모든 과일의 합은 : " + (sum[0] + sum[1] + sum[2]));
 	}
 
 	public static void main(String[] args) {
